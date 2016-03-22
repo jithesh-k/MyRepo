4871b90edcbf88741c3246b3da45e0714cc1b62b
jithesh-k <jithesh.kuzhikali@emc.com>
2016-03-22 09:47:46 +0530

    First Commit

---
+++ FleetManagementDB/FleetManagementDB/Dashboard.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Dashboard.aspx.cs" Inherits="FleetManagementDB.Dashboard" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+     <title></title>
+    <script src="http://maps.googleapis.com/maps/api/js"></script>
+    <script src="Scripts/angular.min.js"></script> 
+    <style type="text/css">
+.bgimg {
+    background-image: url("Images/TruckN.jpg");
+}
+        .auto-style1 {
+            width: 596px;
+            height: 161px;
+        }
+    </style>
+
+
+</head>
+<body>
+    <p>
+        <img class="auto-style1" src="Images/TruckN.jpg" usemap="#truck" /></p>
+        <map name="truck">
+            <area alt="One" shape ="rect" coords="40,158,58,167" href="Map.html" />
+            <area alt="two" shape ="rect" coords="137,159,151,162" href="Map.html" />
+            <area alt="three" shape ="rect" coords="" href="#" />
+        </map>
+   
+</body>
+</html>
---
+++ FleetManagementDB/FleetManagementDB/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FleetManagementDB")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EMC Corporation")]
+[assembly: AssemblyProduct("FleetManagementDB")]
+[assembly: AssemblyCopyright("Copyright © EMC Corporation 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3d390627-0c6f-4dc6-bead-ee8cd3fb4644")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
